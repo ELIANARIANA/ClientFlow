ef1d48ca5df1878950187c6c7530e782f09ce8d5
Elian Ariana <elian.ariana98@hotmail.com>
2026-08-25 20:59:26 +0200

    added new ClientFlow.Tests with .net10.0 TargetFramework 		to fix ('It cannot be referenced by a project that targets 		'.NETFramework,Version=v4.7.2'')

--- ClientFlow.Tests/UnitTest1.cs
+++ ClientFlow.Tests/UnitTest1.cs
@@ -1,14 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-
-namespace ClientFlow.Tests
+﻿namespace ClientFlow.Tests
 {
-    [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Fact]
+        public void Test1()
         {
+
         }
     }
 }
