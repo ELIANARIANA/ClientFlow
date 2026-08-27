31acace307a5f27656319a7910938e2846b7e78f
Elian Ariana <elian.ariana98@hotmail.com>
2026-08-27 20:16:59 +0200

    add Customer entity to ClientFlow.Domain

---
+++ ClientFlow.Domain/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace ClientFlow.Domain.Entities
+{
+    public class Customer
+    {
+        public Guid Id                   { get; set; }
+        public string FirtName           { get; set; } = string.Empty;
+        public string LastName           { get; set; } = string.Empty;
+        public string Email              { get; set; } = string.Empty;
+        public string? Phone             { get; set; }
+        public string? CompanyName       { get; set; }
+        public DateTimeOffset CreatedAt  { get; set; }
+        public DateTimeOffset? UpdatedAt { get; set; }
+    }
+}
