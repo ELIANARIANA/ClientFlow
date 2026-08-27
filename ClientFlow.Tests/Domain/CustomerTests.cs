7ca3674de1c2d5f6af4d33e282014a34d4db80ba
Elian Ariana <elian.ariana98@hotmail.com>
2026-08-27 21:11:21 +0200

    ClientFlow.Tests: add unit tests for Customer entity

---
+++ ClientFlow.Tests/Domain/CustomerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientFlow.Tests.Domain
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void CreateCustomerWithValidData()
+        {
+            var customer = new ClientFlow.Domain.Entities.Customer
+            {
+                Id          = Guid.NewGuid(),
+                FirtName    = "John",
+                LastName    = "Doe",
+                Email       = "john.doe@example.com",
+                Phone       = "123-456-7890",
+                CompanyName = "Example Inc.",
+                CreatedAt   = DateTimeOffset.UtcNow,
+                UpdatedAt   = DateTimeOffset.UtcNow,
+            };
+
+            // Assert
+            Assert.NotEqual(Guid.Empty            , customer.Id         );
+            Assert.Equal   ("John"                , customer.FirtName   );
+            Assert.Equal   ("Doe"                 , customer.LastName   );
+            Assert.Equal   ("john.doe@example.com", customer.Email      );
+            Assert.Equal   ("123-456-7890"        , customer.Phone      );
+            Assert.Equal   ("Example Inc."        , customer.CompanyName);
+            Assert.NotEqual(default               , customer.CreatedAt  );
+            Assert.NotEqual(default               , customer.UpdatedAt  );
+        }
+    }
+}
