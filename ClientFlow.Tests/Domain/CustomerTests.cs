5502f6dd114f13a0b0d2736d3da545ebf97c3a7d
Elian Ariana <elian.ariana98@hotmail.com>
2026-08-29 00:45:07 +0200

    ClientFlow: Use tabs for indentation

--- ClientFlow.Tests/Domain/CustomerTests.cs
+++ ClientFlow.Tests/Domain/CustomerTests.cs
@@ -4,55 +4,55 @@ using System.Text;
 
 namespace ClientFlow.Tests.Domain
 {
-    public class CustomerTests
-    {
-        [Fact]
-        public void CreateCustomerWithValidData()
-        {
-            var customer = new ClientFlow.Domain.Entities.Customer
-            {
-                Id          = Guid.NewGuid(),
-                FirtName    = "John",
-                LastName    = "Doe",
-                Email       = "john.doe@example.com",
-                Phone       = "123-456-7890",
-                CompanyName = "Example Inc.",
-                CreatedAt   = DateTimeOffset.UtcNow,
-                UpdatedAt   = DateTimeOffset.UtcNow,
-            };
+	public class CustomerTests
+	{
+		[Fact]
+		public void CreateCustomerWithValidData()
+		{
+			var customer = new ClientFlow.Domain.Entities.Customer
+			{
+				Id          = Guid.NewGuid(),
+				FirtName    = "John",
+				LastName    = "Doe",
+				Email       = "john.doe@example.com",
+				Phone       = "123-456-7890",
+				CompanyName = "Example Inc.",
+				CreatedAt   = DateTimeOffset.UtcNow,
+				UpdatedAt   = DateTimeOffset.UtcNow,
+			};
 
-            // Assert
-            Assert.NotEqual(Guid.Empty            , customer.Id         );
-            Assert.Equal   ("John"                , customer.FirtName   );
-            Assert.Equal   ("Doe"                 , customer.LastName   );
-            Assert.Equal   ("john.doe@example.com", customer.Email      );
-            Assert.Equal   ("123-456-7890"        , customer.Phone      );
-            Assert.Equal   ("Example Inc."        , customer.CompanyName);
-            Assert.NotEqual(default               , customer.CreatedAt  );
-            Assert.NotEqual(default               , customer.UpdatedAt  );
-        }
+			// Assert
+			Assert.NotEqual(Guid.Empty            , customer.Id         );
+			Assert.Equal   ("John"                , customer.FirtName   );
+			Assert.Equal   ("Doe"                 , customer.LastName   );
+			Assert.Equal   ("john.doe@example.com", customer.Email      );
+			Assert.Equal   ("123-456-7890"        , customer.Phone      );
+			Assert.Equal   ("Example Inc."        , customer.CompanyName);
+			Assert.NotEqual(default               , customer.CreatedAt  );
+			Assert.NotEqual(default               , customer.UpdatedAt  );
+		}
 
-        [Fact]
-        public void CreateCustomerWithOnlyRequiredData()
-        {
-            var customer = new ClientFlow.Domain.Entities.Customer
-            {
-                Id        = Guid.NewGuid(),
-                FirtName  = "John",
-                LastName  = "Doe",
-                Email     = "john.doe@example.com",
-                CreatedAt = DateTimeOffset.UtcNow,
-            };
+		[Fact]
+		public void CreateCustomerWithOnlyRequiredData()
+		{
+			var customer = new ClientFlow.Domain.Entities.Customer
+			{ 
+				Id        = Guid.NewGuid(),
+				FirtName  = "John",
+				LastName  = "Doe",
+				Email     = "john.doe@example.com",
+				CreatedAt = DateTimeOffset.UtcNow,
+			};
 
-            // Assert
-            Assert.NotEqual(Guid.Empty            , customer.Id       );
-            Assert.Equal   ("John"                , customer.FirtName );
-            Assert.Equal   ("Doe"                 , customer.LastName );
-            Assert.Equal   ("john.doe@example.com", customer.Email    );
-            Assert.NotEqual(default               , customer.CreatedAt);
-            Assert.Null(customer.Phone      );
-            Assert.Null(customer.CompanyName);
-            Assert.Null(customer.UpdatedAt  );
-        }
-    }
+			// Assert
+			Assert.NotEqual(Guid.Empty            , customer.Id       );
+			Assert.Equal   ("John"                , customer.FirtName );
+			Assert.Equal   ("Doe"                 , customer.LastName );
+			Assert.Equal   ("john.doe@example.com", customer.Email    );
+			Assert.NotEqual(default               , customer.CreatedAt);
+			Assert.Null(customer.Phone      );
+			Assert.Null(customer.CompanyName);
+			Assert.Null(customer.UpdatedAt  );
+		}
+	}
 }
