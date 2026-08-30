d7f29a0416a1d41f8f38fecd8327150d778525f7
Elian Ariana <elian.ariana98@hotmail.com>
2026-08-30 21:03:09 +0200

    ClientFlow.Infrastructure: add Customers table migration

--- ClientFlow.Infrastructure/AppDbContext.cs
+++ ClientFlow.Infrastructure/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ClientFlow.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace ClientFlow.Infrastructure
 {
@@ -7,8 +9,23 @@ namespace ClientFlow.Infrastructure
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 		{
 		}
+
+		public DbSet<Customer> Customers { get; set; }
+
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
+			base.OnModelCreating(modelBuilder);
+			modelBuilder.Entity<Customer>(entity =>
+			{
+				entity.HasKey(x => x.Id);
+				entity.Property(x => x.FirstName).IsRequired();
+				entity.Property(x => x.LastName).IsRequired();
+				entity.Property(x => x.Email).IsRequired();
+				entity.Property(x => x.Phone);
+				entity.Property(x => x.CompanyName);
+				entity.Property(x => x.CreatedAt).IsRequired();
+				entity.Property(x => x.UpdatedAt);
+			});
 		}
 	}
 }
---
+++ ClientFlow.Infrastructure/Migrations/20260830185731_20260830.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class _20260830 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: true),
+                    CompanyName = table.Column<string>(type: "text", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
