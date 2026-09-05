9f2aa5f38f62023905289983ef9fcf951fc58d21
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-05 22:59:36 +0200

    ClientFlow.Api - CustomersController: add _logger field

--- ClientFlow.Api/Controllers/CustomersController.cs
+++ ClientFlow.Api/Controllers/CustomersController.cs
@@ -10,12 +10,14 @@ namespace ClientFlow.Api.Controllers
 	public class CustomersController : Controller
 	{
 		#region Members
+		private readonly ILogger<CustomersController> _logger;
 		private readonly ICustomerService _customerService;
 		#endregion Members
 
 		#region Constructor
-		public CustomersController(ICustomerService customerService)
+		public CustomersController(ILogger<CustomersController> logger, ICustomerService customerService)
 		{
+			_logger = logger;
 			_customerService = customerService;
 		}
 		#endregion Constructor
@@ -24,10 +26,17 @@ namespace ClientFlow.Api.Controllers
 		[HttpGet("{id}")]
 		public async Task<IActionResult> GetCustomerById(Guid id)
 		{
+			_logger.LogInformation($"Getting customer with ID: '{id}'...");
+
 			var customer = await _customerService.GetCustomerByIdAsync(id);
 			
 			if (customer == null)
+			{
+				_logger.LogWarning($"Customer with ID: {id} not found!");
 				return NotFound();
+			}
+
+			_logger.LogInformation($"Customer with ID: {id} retrieved successfully.");
 
 			return Ok(customer);
 		}
@@ -35,7 +44,12 @@ namespace ClientFlow.Api.Controllers
 		[HttpGet]
 		public async Task<IActionResult> GetCustomers()
 		{
+			_logger.LogInformation("Getting customers...");
+
 			var customers = await _customerService.GetCustomersAsync();
+
+			_logger.LogInformation($"Retrieved {customers.Count()} customers.");
+
 			return Ok(customers);
 		}
 
@@ -43,9 +57,18 @@ namespace ClientFlow.Api.Controllers
 		public async Task<IActionResult> AddCustomer([FromBody]Customer customer)
 		{
 			if (!IsValidCustomer(customer))
+			{
+				_logger.LogWarning("Invalid customer data received!");
+
 				return BadRequest("Invalid customer data.");
+			}
+				
+
+			_logger.LogInformation("Adding new customer...");
 
 			await _customerService.AddCustomerAsync(customer);
+
+			_logger.LogInformation($"Customer with ID: {customer.Id} created successfully.");
 
 			return Ok(new { Message = "Customer created successfully.", Customer = customer });
 		}
@@ -54,12 +77,23 @@ namespace ClientFlow.Api.Controllers
 		public async Task<IActionResult> UpdateCustomer(Guid id,Customer customer)
 		{
 			if (!IsValidCustomer(customer))
+			{
+				_logger.LogWarning("Invalid customer data received!");
+
 				return BadRequest("Invalid customer data.");
+			}
+				
+			_logger.LogInformation($"Updating customer with ID: '{id}'...");
 
 			var result = await _customerService.UpdateCustomerAsync(id, customer);
 
 			if (result == null)
+			{
+				_logger.LogWarning($"Customer with ID: {id} not found!");
 				return NotFound();
+			}
+			
+			_logger.LogInformation($"Customer with ID: {id} updated successfully.");
 
 			return Ok(new { Message = "Customer updated successfully.", Customer = customer });
 		}
@@ -67,10 +101,17 @@ namespace ClientFlow.Api.Controllers
 		[HttpDelete("{id}")]
 		public async Task<IActionResult> DeleteCustomer(Guid id)
 		{
+			_logger.LogInformation($"Deleting customer with ID: '{id}'...");
+
 			var result = await _customerService.DeleteCustomerAsync(id);
 
 			if (result == null)
+			{
+				_logger.LogWarning($"Customer with ID: {id} not found!");
 				return NotFound();
+			}
+
+			_logger.LogInformation($"Customer with ID: {id} deleted successfully.");
 
 			return Ok(new { Message = "Customer deleted successfully.", Customer = result });
 		}
