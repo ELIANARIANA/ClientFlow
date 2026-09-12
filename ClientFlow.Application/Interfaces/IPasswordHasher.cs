0d5355f080c35abca0d4ad991ddc7a2e243349b6
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-12 23:26:44 +0200

    ClientFlow - Add BCrypt PasswordHasher implementation

---
+++ ClientFlow.Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientFlow.Application.Interfaces
+{
+	public interface IPasswordHasher
+	{
+		string Hash(string password);
+		bool Verify(string password, string hash);
+	}
+}
---
+++ ClientFlow.Infrastructure/Security/PasswordHasher.cs
@@ -0,0 +1,19 @@
+﻿using ClientFlow.Application.Interfaces;
+
+namespace ClientFlow.Infrastructure.Security
+{
+	public class PasswordHasher : IPasswordHasher
+	{
+		public string Hash(string password)
+		{
+			// Implementation for hashing password
+			return BCrypt.Net.BCrypt.HashPassword(password);
+		}
+
+		public bool Verify(string password, string hash)
+		{
+			// Implementation for verifying password
+			return BCrypt.Net.BCrypt.Verify(password, hash);
+		}
+	}
+}
