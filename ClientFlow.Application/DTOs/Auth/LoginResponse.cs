f2bfe2113f541c972c5354408a02b35d6a97a563
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-13 20:31:55 +0200

    ClientFlow.Application - add DTOs for Login

---
+++ ClientFlow.Application/DTOs/Auth/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace ClientFlow.Application.DTOs.Auth
+{
+	public class LoginResponse
+	{
+		public string AccessToken { get; set; } = string.Empty;
+		public DateTimeOffset ExpiresAt { get; set; }
+	}
+}
