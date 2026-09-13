6c409a9fae3ea2c58e46acb0fb4c6323b72caec7
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-13 20:19:48 +0200

    ClientFlow - add JWT token service

---
+++ ClientFlow.Application/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,9 @@
+﻿using ClientFlow.Domain.Entities;
+
+namespace ClientFlow.Application.Interfaces
+{
+	public interface IJwtTokenService
+	{
+		string CreateToken(User user);
+	}
+}
---
+++ ClientFlow.Infrastructure/Security/JwtTokenService.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+using ClientFlow.Application.Interfaces;
+using ClientFlow.Domain.Entities;
+
+namespace ClientFlow.Infrastructure.Security
+{
+	public class JwtTokenService : IJwtTokenService
+	{
+		#region Members
+		private readonly IConfiguration _configuration;
+		#endregion Members
+
+		#region Constructor
+		public JwtTokenService(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		#endregion Constructor
+
+		public string CreateToken(User user)
+		{
+			var key = _configuration["Jwt:Key"]
+				?? throw new InvalidOperationException(
+				"Jwt Key is not configured");
+
+			var issuer = _configuration["Jwt:Issuer"]
+				?? throw new InvalidOperationException(
+				"Jwt Issuer is not configured");
+
+			var audience = _configuration["Jwt:Audience"]
+				?? throw new InvalidOperationException(
+				"Jwt Audience is not configured");
+
+			var expirationMinutes = int.TryParse(
+				_configuration["Jwt:ExpirationMinutes"], out var minutes)
+				? minutes
+				: throw new InvalidOperationException(
+				"Jwt ExpirationMinutes is not configured");
+
+			var claims = new List<Claim>
+			{
+				new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+				new(JwtRegisteredClaimNames.Email, user.Email),
+				new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+				new(ClaimTypes.Email, user.Email),
+				new(ClaimTypes.Role, user.Role),
+			};
+
+			var seurityKey = new SymmetricSecurityKey(
+				Encoding.UTF8.GetBytes(key));
+
+			var credentials = new SigningCredentials(
+				seurityKey,
+				SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				issuer: issuer,
+				audience: audience,
+				claims: claims,
+				expires: DateTime.UtcNow.AddMinutes(expirationMinutes),
+				signingCredentials: credentials);
+
+			return new JwtSecurityTokenHandler()
+				.WriteToken(token);
+		}
+	}
+}
