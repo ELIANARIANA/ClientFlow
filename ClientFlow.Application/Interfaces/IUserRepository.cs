c256712bcf19900492269a97c45e08245669047c
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-14 22:03:01 +0200

    ClientFlow.Infrastructure - add user repository

---
+++ ClientFlow.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using ClientFlow.Domain.Entities;
+
+namespace ClientFlow.Application.Interfaces
+{
+	public interface IUserRepository
+	{
+		Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken);
+		Task AddAsync(User user, CancellationToken cancellationToken);
+	}
+}
---
+++ ClientFlow.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using ClientFlow.Application.Interfaces;
+using ClientFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientFlow.Infrastructure.Repositories
+{
+	public class UserRepository : IUserRepository
+	{
+		#region Members
+		private readonly AppDbContext _context;
+		#endregion Members
+
+		#region Constructor
+		public UserRepository(AppDbContext context)
+		{
+			_context = context;
+		}
+		#endregion Constructor
+
+		#region Public Methods
+		public async Task<User?> GetByEmailAsync(string email, CancellationToken cancellationToken = default)
+		{
+			return await _context.Users.FirstOrDefaultAsync(x => x.Email == email, cancellationToken);
+		}
+
+		public async Task AddAsync(User user, CancellationToken cancellationToken = default)
+		{
+			await _context.Users.AddAsync(user, cancellationToken);
+
+			await _context.SaveChangesAsync(cancellationToken);
+		}
+		#endregion Public Methods
+	}
+}
