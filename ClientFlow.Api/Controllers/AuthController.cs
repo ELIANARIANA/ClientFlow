39c0b6c13ec5bf11bec201bc203d5f685754579f
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-16 23:03:43 +0200

    ClientFlow.Api: add AuthController

---
+++ ClientFlow.Api/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using ClientFlow.Application.DTOs.Auth;
+using ClientFlow.Application.Services;
+
+namespace ClientFlow.Api.Controllers
+{
+	/// <summary>
+	/// Controller for Authorization.
+	/// </summary>
+	[ApiController]
+	[Route("api/Auth")]
+	public class AuthController : ControllerBase
+	{
+		#region Members
+		private readonly ILogger<AuthController> _logger;
+		private readonly AuthService _authService;
+		#endregion Members
+
+		#region Constructor
+		/// <summary>
+		/// Initializes a new instance of the <see cref="AuthController"/> class.
+		/// </summary>
+		/// <param name="logger"></param>
+		/// <param name="authService"></param>
+		public AuthController(ILogger<AuthController> logger, AuthService authService)
+		{
+			_logger      = logger;
+			_authService = authService;
+		}
+		#endregion Constructor
+
+		#region Public Methods
+		/// <summary>
+		/// user login.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <param name="cancellationToken"></param>
+		/// <returns></returns>
+		[AllowAnonymous]
+		[HttpPost("Login")]
+		public async Task<ActionResult<LoginResponse>> Login(LoginRequest request, CancellationToken cancellationToken)
+		{
+			var response = await _authService.LoginAsync(request, cancellationToken);
+
+			return Ok(response);
+		}
+		#endregion Public Methods
+	}
+}
