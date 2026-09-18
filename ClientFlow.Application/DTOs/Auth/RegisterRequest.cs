4b5f63c757a4ef3d1e1675af43a1b6662f7c4821
Elian Ariana <elian.ariana98@hotmail.com>
2026-09-18 23:43:38 +0200

    Alignment fix

--- ClientFlow.Application/DTOs/Auth/RegisterRequest.cs
+++ ClientFlow.Application/DTOs/Auth/RegisterRequest.cs
@@ -5,7 +5,7 @@ namespace ClientFlow.Application.DTOs.Auth
 	public class RegisterRequest
 	{
 		[EmailAddress]
-		public string Email { get; set; } = string.Empty;
+		public string Email    { get; set; } = string.Empty;
 		public string Password { get; set; } = string.Empty;
 	}
 }
